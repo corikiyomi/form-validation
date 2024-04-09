797ff64c9752ac93bbbbaa042efe808a83e875fe
Cori Komiyama <corikiyomi@gmail.com>
2024-04-09 07:58:29 -0700

    Restructure register 2.js

--- register 2.js
+++ register 2.js
@@ -9,72 +9,51 @@ let passConfirm = document.getElementById("passwordConfirm");
 let formErrors = document.getElementById("formErrors");
 let submit = document.getElementById("submit");
 
-form = false;
-
 // create function checkForm()
 function checkForm(e) {
    // prevent default for submission
    e.preventDefault();
 
+   // Validation Steps
+      // save each invalid input element in a list ?
+      // or add class "error" to each invalid element
+
+   // 1. Ensure a full name with length greater than or equal to 1 was provided
+      // otherwise, display "Missing full name"
+   // 2. Ensure that an email address was provided and email address matches the regular expression:
+   // /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/
+      // otherwise, display "Invalid or missing email address."
+   // 3. Ensure the password has 10 to 20 characters
+      // otherwise, display "Password must be between 10 and 20 characters."
+   // 4. Ensure the password contains at least one lowercase character (use a regular expression)
+      // otherwise, display "Password must contain at least one lowercase character."
+   // 5. Ensure the password contains at least one uppercase character (use a regular expression)
+      // otherwise, display "Password must contain at least one uppercase character."
+   // 6. Ensure the password contains at least one digit (use a regular expression)
+      // otherwise, display "Password must contain at least one digit."
+   // 7. Ensure the password and confirmation password match
+      // otherwise, display "Password and confirmation password don't match."
+
+
    // if form validation errors exist:
-   if (form == false) {
+   if () {
 
       // Display the formErrors <div> by removing the hide class
-      formErrors.classList.remove("hide");
 
-      // Display each of the associated error messages in the formErrors <div> using an unordered list. The error message must be displayed in the order the validation is performed, following the order specified below
-      formErrors.innerHTML += error;
+      // Display each of the associated error messages in the formErrors <div> using an unordered list. 
+      // The error message must be displayed in the order the validation is performed, following the order specified below
 
       // Add the error class to each <input> element with invalid input
-      let invalidInput = document.getElementsByClassName("invalid");
-      for (let elem of invalidInput) {
-         elem.classList.add("error");
-      }
 
-   } else if (form == true) { // if no form validation errors exist:
+   } else { // if no form validation errors exist:
+      
       // Hide the formErrors <div> by adding the hide class
-      formErrors.classList.add("hide");
 
       // Remove the error class from all text, email, and password <input> elements
-      fullName.classList.remove("error");
-      email.classList.remove("error");
-      password.classList.remove("error");
 
    }
 }
 
-// Perform the following form validations in the order provided and display all error messages that apply:
-
-
-   // ensure a full name with length greater than or equal to 1 was provided
-
-      // otherwise, display "Missing full name"
-
-   // Ensure that an email address was provided and email address matches the regular expression:
-   // /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/
-
-   
-      // otherwise, display "Invalid or missing email address."
-
-   // Ensure the password has 10 to 20 characters
-
-      // otherwise, display "Password must be between 10 and 20 characters."
-
-   // Ensure the password contains at least one lowercase character (use a regular expression)
-
-      // otherwise, display "Password must contain at least one lowercase character."
-
-   // Ensure the password contains at least one uppercase character (use a regular expression)
-
-      // otherwise, display "Password must contain at least one uppercase character."
-
-   // Ensure the password contains at least one digit (use a regular expression)
-
-      // otherwise, display "Password must contain at least one digit."
-
-   // Ensure the password and confirmation password match
-
-      // otherwise, display "Password and confirmation password don't match."
 
 document.getElementById("submit").addEventListener("click", function(event) {
    checkForm();
